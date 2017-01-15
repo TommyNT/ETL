7685c7d35eb60d6ff4987f4d0766f026e64ff313
Duong Nguyen Tuan <TommyNT@MacBook-Pro-Duong.local>
2017-01-15 16:10:59 +0100

    back-end simply ETL done

--- etl.php
+++ etl.php
@@ -9,7 +9,7 @@ $html = file_get_html('http://www.ceneo.pl/'.$_POST["productId"]);
 
 // Transform
 // Zaciągnięcie rodzaju, marki, modelu i dodatkowych uwag urządzenia 
-$productType = $html->find('.breadcrumbs span', 2)->plaintext;
+$productType = trim($html->find('.breadcrumbs span', 2)->plaintext);
 echo $productType;
 $productBrand = $html->find('li.attr-value.a.dotted-link', 3)->plaintext;
 echo $productBrand;
@@ -18,16 +18,17 @@ echo $productModel;
 $productAdditional = $html->find('.ProductSublineTags', 0)->plaintext;
 echo $productAdditional;
 
-// Zaciągnięcie wszystkich opinii ze strony
+// Zaciągnięcie wszystkich opinii ze strony i sformatowanie
 foreach($html->find('li.product-review') as $article) {
-	$item['cons'] = $article->find('div.cons-cell', 0)->plaintext;
-	$item['pros'] = $article->find('div.pros-cell', 0)->plaintext;
-	$item['review'] = $article->find('p.product-review-body', 0)->plaintext;
-	$item['score'] = $article->find('span.review-score-count', 0)->plaintext;
-    $item['reviewer']     = $article->find('div.product-reviewer', 0)->plaintext;
-    $item['date']     = $article->find('span.review-time', 0)->plaintext;
-    $item['recommended']    = $article->find('em.product-recommended', 0)->plaintext;
-    $item['votes']    = $article->find('span.js_product-review-usefulness', 0)->plaintext;
+	$item['cons'] = trim(str_replace("Wady", "", preg_replace('/\s+/', ' ', $article->find('div.cons-cell', 0)->plaintext)));
+	$item['pros'] = trim(str_replace("Zalety", "", preg_replace('/\s+/', ' ', $article->find('div.pros-cell', 0)->plaintext)));
+	$item['review'] = trim(preg_replace('/\s+/', ' ', $article->find('p.product-review-body', 0)->plaintext));
+	$item['score'] = trim($article->find('span.review-score-count', 0)->plaintext);
+    $item['reviewer']     = trim($article->find('div.product-reviewer', 0)->plaintext);
+    $item['date']     = trim($article->find('span.review-time', 0)->plaintext);
+    $item['recommended']    = trim($article->find('em.product-recommended', 0)->plaintext);
+    $item['vote-yes']    = trim(preg_replace('/\s+/', ' ', $article->find('button.vote-yes', 0)->plaintext));
+    $item['vote-no']    = trim(preg_replace('/\s+/', ' ', $article->find('button.vote-no', 0)->plaintext));
     $articles[] = $item;
 }
 
@@ -43,9 +44,36 @@ if ($connect->connect_errno!=0) {
     echo "Error: ".$connect->connect_errno;
 }
 else{
+	//Zasilenie tabeli produkt
+	$connect->query("SET NAMES 'utf8'");
 	$connect->query("INSERT INTO produkt VALUES (NULL, '$productType', '$productBrand', '$productModel', '$productAdditional')");
+	//Zaciągnięcie ID ostatnio dodanego produktu
+	$produkt_id = $connect->insert_id;
 
-	echo "Zasilono bazę danych :)";
+	//Zasilenie tabeli opinia
+	if(is_array($articles)){
+
+	    $sql = "INSERT INTO opinia (id, produkt_id, wady_produktu, zalety_produktu, podsumowanie, liczba_gwiazdek, autor_opinii, data_wystawienia_opinii, polecam_nie_polecam, opinia_przydatna, opinia_nieprzydatna) VALUES ";
+	    //Rozdzielenie elementów w tablicy dotyczących opinii
+	    $valuesArr = array();
+	    foreach($articles as $row){
+
+
+	        $wady_produktu = mysql_real_escape_string( $row['cons'] );
+	        $zalety_produktu = mysql_real_escape_string( $row['pros'] );
+	        $podsumowanie = mysql_real_escape_string( $row['review'] );
+	        $liczba_gwiazdek = mysql_real_escape_string( $row['score'] );
+	        $autor_opinii = mysql_real_escape_string( $row['reviewer'] );
+	        $data_wystawienia_opinii = mysql_real_escape_string( $row['date'] );
+	        $polecam_nie_polecam = mysql_real_escape_string( $row['recommended'] );
+	        $opinia_przydatna = mysql_real_escape_string( $row['vote-yes'] );
+			$opinia_nieprzydatna = mysql_real_escape_string( $row['vote-no'] );
+	        $valuesArr[] = "(NULL, '$produkt_id','$wady_produktu', '$zalety_produktu', '$podsumowanie', '$liczba_gwiazdek', '$autor_opinii', '$data_wystawienia_opinii', '$polecam_nie_polecam', '$opinia_przydatna','$opinia_nieprzydatna' )";
+    }
+	    $sql .= implode(',', $valuesArr);
+	    var_dump($sql);
+	    $connect->query($sql); 
+	}
 
 	$connect->close();
 
